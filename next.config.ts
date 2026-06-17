5df8057e15aa43484a568a0b7b03ddfa7feb97f1
Jhoseph Duor <hgdev.tb@gmail.com>
2026-06-17 17:16:19 -0500

    feat: cambio de next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  allowedDevOrigins: ["192.168.20.209"],
+  allowedDevOrigins: ["192.168.20.213"],
 };
 
 export default nextConfig;
